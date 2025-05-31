ef66ae199d3c61021ba5e383fe119c4f2ce3bd24
MoYuanJun <moyuanjun925@gmail.coml>
2025-05-31 20:04:34 +0800

    feat: 删除无效代码组件

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import { Button } from '@heroui/react';
-import Icon from '@/components/Icon';
+'use client';
 
 export default function Home() {
-  return (
-    <div className="h-screen w-screen">
-      <Button color="primary">Button</Button>
-      <Icon
-        name="icon-send-fill"
-        className="text-red-500"
-      />
-    </div>
-  );
+  return <div className="h-screen w-screen">Button</div>;
 }
