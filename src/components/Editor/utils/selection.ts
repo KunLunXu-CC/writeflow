cc1f651697b368ad642b3fd241fd80a02bc72e8e
墨渊君 <moyuanjun925@gmail.com>
2025-07-14 13:44:34 +0800

    chore: 内联代码块退出逻辑处理

--- src/components/Editor/utils/selection.ts
+++ src/components/Editor/utils/selection.ts
@@ -1,33 +1,11 @@
 import { EditorState } from 'prosemirror-state';
 
 /**
- * 判断光标是否在当前段落的第一个字符(未测试)
+ * 判断光标是否在当前段落的第一个字符
  * @param state - 编辑器状态
  * @returns 是否在当前段落的第一个字符
  */
 export const isStartInParagraph = (state: EditorState) => {
-  const $from = state.selection.$from;
-  const isAtStart = $from.parentOffset === 0;
-  return isAtStart;
-};
-
-/**
- * 判断光标是否在文档最后一行(未测试)
- * @param state - 编辑器状态
- * @returns 是否在文档最后一行
- */
-export const isLastLineInDoc = (state: EditorState) => {
-  const $from = state.selection.$from;
-  const isAtEnd = $from.parentOffset === $from.parent.nodeSize - 2;
-  return isAtEnd;
-};
-
-/**
- * 判断光标是否在文档第一行(未测试)
- * @param state - 编辑器状态
- * @returns 是否在文档第一行
- */
-export const isFirstLineInDoc = (state: EditorState) => {
   const $from = state.selection.$from;
   const isAtStart = $from.parentOffset === 0;
   return isAtStart;
