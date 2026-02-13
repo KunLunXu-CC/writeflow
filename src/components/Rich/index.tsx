e7c232978e33bfc44f901c99011928d9cfa35943
墨渊君 <moyuanjun925@gmail.com>
2026-02-13 17:54:07 +0800

    chore: dark theme

--- src/components/Rich/index.tsx
+++ src/components/Rich/index.tsx
@@ -2,8 +2,10 @@ import { WriteFlowContext } from '@/components/WriteFlowContext';
 import { useWriteFlow } from '../hooks/useWriteFlow';
 import { RichBubbleMenu } from './RichBubbleMenu';
 import { Textarea } from '@heroui/react';
+import { THEME } from '../WriteFlow/types';
 
 import '@/components/WriteFlow/styles';
+import './index.scss';
 
 interface EditorProps {
   className?: string;
@@ -19,10 +21,26 @@ export const WriteFlow = (props: EditorProps) => {
     }
   };
 
+  const handDarkTheme = () => {
+    if (writeFlow) {
+      writeFlow.setTheme(THEME.DARK);
+    }
+  };
+
   return (
     <>
-      <div className="w-full max-w-[600px] py-6">
-        <Textarea onValueChange={handleChange} />
+      <div className="w-full max-w-[600px] py-6 flex gap-2">
+        <div className="flex-1">
+          <Textarea
+            minRows={1}
+            onValueChange={handleChange}
+          />
+        </div>
+        <div
+          className="flex-none"
+          onClick={handDarkTheme}>
+          123
+        </div>
       </div>
       <WriteFlowContext.Provider value={writeFlow}>
         <div
