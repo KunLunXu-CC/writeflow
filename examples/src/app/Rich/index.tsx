d65bcc12ab3f251d80331447c7cf1a01d5c84968
墨渊君 <moyuanjun925@gmail.com>
2026-03-16 13:50:20 +0800

    feat: react add options

--- examples/src/app/Rich/index.tsx
+++ examples/src/app/Rich/index.tsx
@@ -2,16 +2,52 @@ import { FC, ReactNode } from 'react';
 // import { RichBubbleMenu } from './RichBubbleMenu';
 import { useCreateWriteFlow, WriteFlowContext } from '@kunlunxu/wf-react';
 
-// import '@/components/WriteFlow/styles';
+import { Heading } from '@kunlunxu/wf-extension-heading';
+import { TaskList, TaskItem, ListItem, BulletList, OrderedList } from '@kunlunxu/wf-extension-list';
+import { Table, TableCell, TableHeader, TableRow } from '@kunlunxu/wf-extension-table';
+import { Base } from '@kunlunxu/wf-extension-base';
+import { Image } from '@kunlunxu/wf-extension-image';
+import { HorizontalRule } from '@kunlunxu/wf-extension-horizontal-rule';
+import { Paragraph } from '@kunlunxu/wf-extension-paragraph';
+import { HardBreak } from '@kunlunxu/wf-extension-hard-break';
+import { Blockquote } from '@kunlunxu/wf-extension-blockquote';
+import { CodeBlock, InlineCode } from '@kunlunxu/wf-extension-code';
+import { Link } from '@kunlunxu/wf-extension-link';
+import { Markdown } from '@kunlunxu/wf-extension-markdown';
 
 interface EditorProps {
   className?: string;
   children?: ReactNode;
 }
 
+const extensions = [
+  Base,
+  HorizontalRule,
+  Heading,
+  Blockquote,
+  ListItem,
+  TaskList,
+  TaskItem,
+  BulletList,
+  OrderedList,
+  Table,
+  TableRow,
+  TableCell,
+  TableHeader,
+  CodeBlock,
+  InlineCode,
+  HardBreak,
+  Paragraph,
+  Image,
+  Markdown,
+  Link,
+];
+
 export const Rich: FC<EditorProps> = (props: EditorProps) => {
   const { className, children } = props;
-  const { writeFlow, writeFlowDomRef } = useCreateWriteFlow();
+  const { writeFlow, writeFlowDomRef } = useCreateWriteFlow({
+    extensions,
+  });
 
   return (
     <WriteFlowContext.Provider value={writeFlow}>
